ae1bfa3cd9880d82778c7c506d6037abb3d29328
pkutaj <pkutaj@gmail.com>
2020-04-01 04:50:07 +0200

    SQL > WHERE

---
+++ assets/2020-03-31.js
@@ -0,0 +1,26 @@
+/* the concern is to return first and last item of a targer within an ascending array */
+
+/* INITS */
+const nums = [5, 7, 7, 8, 8, 10]
+
+/* WORK */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    let matchedPositions = [];
+    let limitPositions = new Array(2)
+    let i = 0;
+    let numsSize = nums.length;
+    for (i; i < numsSize; i++) {
+        if (nums[i] === target) matchedPositions.push(i)
+    };
+    if (matchedPositions.length) {
+        return limitPositions = [matchedPositions[0], matchedPositions[matchedPositions.length - 1]]
+    } else {return limitPositions = [-1, -1]}
+
+}
+/* TEST */
+console.log(searchRange(nums, 6))
