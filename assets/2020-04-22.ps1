9e9e6a4e51bf1d45b0611ec9d7ab292ebefccaa7
pkutaj <pkutaj@gmail.com>
2020-04-23 06:20:14 +0200

    asset update

---
+++ assets/2020-04-22.ps1
@@ -0,0 +1,23 @@
+# the aim of the script is to create a ticket space
+
+
+
+function Create-TicketSpace {
+    [CmdletBinding()]
+    param (
+        [string]$TicketId,
+        [string]$TicketHeading
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
