6ad6c41b764a1e962dd94a04a7a37214bfcf6314
pkutaj <pkutaj@gmail.com>
2020-04-30 09:56:48 +0200

    2020-04-30 start-recording -WORKING

--- assets/2020-04-30-start-recording.ps1
+++ assets/2020-04-30-start-recording.ps1
@@ -4,10 +4,9 @@ function Start-Recording {
         [string]$filePath
     )
     
-    $argumentList = '-rtbufsize 150M -f dshow -framerate 30 -i video="screen-capture-recorder" -c:v libx264 -r 30 -preset ultrafast -tune zerolatency -crf 28 -pix_fmt yuvgit 420p -movflags +faststart -y "c:\Users\Admin\Downloads\output.mp4"'
+    $argumentList = '-rtbufsize 150M -f dshow -framerate 30 -i video="screen-capture-recorder" -c:v libx264 -r 30 -preset ultrafast -tune zerolatency -crf 28 -pix_fmt yuv420p -movflags +faststart -y "c:\Users\Admin\Downloads\output.mp4"'
 
         Start-Process c:\Users\Admin\Documents\ShareX\Tools\ffmpeg.exe -ArgumentList $argumentList
- <# this should dissapear #>
 }
 
 Start-Recording
