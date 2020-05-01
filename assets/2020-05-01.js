1da8a9dcb788d878e4a54b90af15cd57de44e7ac
pkutaj <pkutaj@gmail.com>
2020-05-01 06:36:42 +0200

    2020-05-01.js twosum -RESTORE

---
+++ assets/2020-05-01.js
@@ -0,0 +1,21 @@
+/* the aim of this kata is to solve https://leetcode.com/problems/two-sum/ */
+
+/* INITS */
+const nums = [11,12]
+const target = 23;
+let term1 = undefined;
+let term1index = undefined;
+let term2 = undefined;
+let term2index = undefined; 
+
+/* WORK */
+function getTwoSum(nums, target) {
+    i = 0;
+    numsSize = nums.length
+
+    if (target > Math.max(...nums)) return "target is greater"
+
+    };
+
+/* EXPORT */
+module.exports.getTwoSum = getTwoSum;
