50bc9f5e933960caaa4346e7d4a6561125b08db6
pkutaj <pkutaj@gmail.com>
2020-05-05 06:13:03 +0200

    asset -update

--- assets/2020-05-05.js
+++ assets/2020-05-05.js
@@ -1,32 +1,28 @@
 /* the aim of this kata is to solve https://leetcode.com/problems/two-sum/ */
 
 /* INITS */
-const nums = [1,2,3,4,5,6,7,8,9,10]
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const target = 10
 
+const getMinValue = nums => { return Math.min(...nums) }
 
-/* WORK */
 function getTwoSum(nums, target) {
     i = 0;
     numsSize = nums.length
     let pairs = [];
     let numList = []; //?
-    let term1 = undefined;
-    let term1index = undefined;
-    let term2 = undefined;
-    let term2index = undefined;
-
+    if (target < getMinValue(nums)) return "target is smaller" 
     for (i; i < nums.length; i++) {
         let term1 = nums[i];
         let term2 = target - term1;
-        if(numList.includes(term2)) {
-            pairs.push([term1, term2]);
-        }
+        if (numList.includes(term2)) pairs.push([term1, term2]);
         numList.push(term1);
     }
-    return pairs
-};
+    if (pairs.length) return pairs
+        else return "no terms add up to target"
+}
 
 console.log(getTwoSum(nums, target))
+
 /* EXPORT */
 module.exports.getTwoSum = getTwoSum;
