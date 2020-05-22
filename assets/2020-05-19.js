de9c043305f6b3d28cead836780f7b9a7a9bea98
pkutaj <pkutaj@gmail.com>
2020-05-22 07:35:21 +0200

    assets 2020-05-22

--- assets/2020-05-19.js
+++ assets/2020-05-19.js
@@ -1,15 +1,24 @@
-/* The concern is to swap integers using to the "to the array and back" approach */
-const checkInt32 = x => {
-    if (x >= Math.pow(2, 31) || x < Math.pow(-2, 31)) { return true }
-    else { return false }
-}
-const checkNegative = x => x < 0 ? true : false;
-const checkZeroes = x => x % 10 === 0 ? true : false;
+/* The concern is to swap integers using to the "to the array and back approach */
+
+class arrayFromInteger {
+    constructor(x) {
+        Array.from(String(x), Number)
+    }
+};
+
+const makeArray = x => Array.from(String(x), Number)
 const removeZeroes = x => {
     while (checkZeroes(x)) {
         x = x / 10;
     }
     return x;
+}
+const checkZeroes = x => x % 10 === 0 ? true : false;
+const makePositive = x => Math.abs(x);
+const checkNegative = x => x < 0 ? true : false;
+const checkInt32 = x => {
+    if (x >= Math.pow(2, 31) || x < Math.pow(-2, 31)) { return true }
+    else { return false }
 }
 
 var reverse = function (x) {
@@ -18,27 +27,28 @@ var reverse = function (x) {
     if (isNotInt32 || x === 0) return 0
 
     let isNegative = checkNegative(x);
-    if (isNegative) x = Math.abs(x)
+    if (isNegative) x = makePositive(x);
 
     let isEndedWithZeroes = checkZeroes(x)
     if (isEndedWithZeroes) x = removeZeroes(x);
 
-    let intToArr = Array.from(String(x), Number)
+
+    let arrFromX = makeArray(x);
     let i = 0;
-    let lastIndex = intToArr.length - 1
-    let size = intToArr.length;
+    let lastIndex = arrFromX.length - 1;
+    let size = arrFromX.length;
     let mid = Math.floor(size / 2);
 
     while (i < mid) {
         let helper = 0;
-        helper = intToArr[i];
-        intToArr[i] = intToArr[lastIndex]
-        intToArr[lastIndex] = helper;
+        helper = arrFromX[i];
+        arrFromX[i] = arrFromX[lastIndex]
+        arrFromX[lastIndex] = helper;
         i++;
         lastIndex--;
     }
 
-    let absResult = Number(intToArr.join(""));
+    let absResult = Number(arrFromX.join(""));
     isNotInt32 = checkInt32(absResult);
     if (isNotInt32) return 0
     if (isNegative) return -Math.abs(absResult);
