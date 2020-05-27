fadc6f49a8427793661288118f809ad467e06969
pkutaj <pkutaj@gmail.com>
2020-05-27 10:18:43 +0200

    assets -U

--- assets/2020-05-25.js
+++ assets/2020-05-25.js
@@ -1,7 +1,14 @@
-function isPalindrome(x){
-let arr = Array.from(String(x), Number)
-let i = 0
-let lastI = arr.length-1
-return [arr[i], arr[lastI]];
+function isPalindrome(x) {
+    let arr = Array.from(String(x), Number)
+    let i = 0
+    let lastI = arr.length - 1
+    let result;
+    if(arr.length === 1) return true
+    while (i < lastI) {
+        if(arr[i] !== arr[lastI]) return result = false;
+        i++;
+        lastI--;
+    }
+    return result = true;
 }
-module.exports.test = isPalindrome;
+module.exports = isPalindrome;
--- assets/2020-05-25.test.js
+++ assets/2020-05-25.test.js
@@ -1,8 +1,29 @@
-const abc = require("./2020-05-25");
+const isPalindrome = require("./2020-05-25");
 describe('isPalindrome', () => {
-    test('given an int (12345), it should return "[1,5]"', () => {
+    test('given an int with different first and last digist (12345), it should return "false"', () => {
         let int = 12345
-        let result = abc.test(int);
-        expect(result).toEqual([1,5]);
+        let result = isPalindrome(int);
+        expect(result).toEqual(false);
     });
+
+    test('given an int same limit digits, with different second and last-1 digits (12331), it should return "false"', () => {
+        let int = 12331;
+        let result = isPalindrome(int);
+        expect(result).toEqual(false);
+    });
+
+    test('given an int with 1 digit (0), it should return "true"', () => {
+        let int = 0;
+        let result = isPalindrome(int);
+        expect(result).toEqual(true);
+    });
+
+    test('given 1000021, it should return false', () => {
+        let int = 1000021;
+        let result = isPalindrome(int);
+        expect(result).toEqual(false)
+        
+    });
+
+
 });
