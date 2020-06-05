8a4b650ae204d7381cb4710f20570b3c5d933b32
pkutaj <pkutaj@gmail.com>
2020-06-05 04:21:33 +0000

    assets

--- assets/2020-06-01.js
+++ assets/2020-06-01.js
@@ -1,15 +1,28 @@
-function romanToInt(roman) {
-let table = {
-    "I" : 1, 
-    "V": 5, 
-    "X": 10,
-    "L": 50, 
-    "C": 100, 
-    "D": 500,
-    "M": 1000
+const expandRoman = roman => {
+    const abbr = [
+        ["IV", "IIII"],
+        ["XL", "LLLL"]
+    ]
+    let i = 0;
+    let romanExpanded;
+    for (i; i < 2; i++) {
+        romanExpanded = roman.replace(abbr[i][0], abbr[i][1])
+    };    
+    return romanExpanded;
 }
+function romanToInt(roman) {
+    let romanExpanded = expandRoman(roman);
+    let table = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
-return Array.from(String(roman), roman => table[roman]).reduce((a,b)=> a+b, 0)
+    return Array.from(String(romanExpanded), romanExpanded => table[romanExpanded]).reduce((a, b) => a + b, 0)
 
 }
 module.exports = romanToInt;
--- assets/2020-06-01.test.js
+++ assets/2020-06-01.test.js
@@ -1,6 +1,6 @@
 const romanToInt = require("./2020-06-01")
-describe('romanToInt', () => {
-    test('given I, it shoult return [1]', () => {
+/* describe('romanToInt', () => {
+    test('given I, it shoult return 1', () => {
     let roman = "I";
     let result = romanToInt(roman);
     expect(result).toEqual(1);
@@ -17,4 +17,16 @@ test('given ""VIIII"", it should return 9', () => {
     let roman = "VIIII";
     let result = romanToInt(roman);
     expect(result).toEqual(9);
+});
+
+test('give "XXIV", it shoud return 24', () => {
+    let roman = "XXIV"
+    let result = romanToInt(roman);
+    expect(result).toEqual(24)
+}); */
+
+test('give "XLIV, it should return 44', () => {
+    let roman = "XLIV"
+    let result = romanToInt(roman);
+    expect(result).toEqual(44)
 });
