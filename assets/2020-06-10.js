35a8647d62f26a90e975e4254f3f0a24866cbee9
pkutaj <pkutaj@gmail.com>
2020-06-12 03:40:02 +0000

    assets

--- assets/2020-06-10.js
+++ assets/2020-06-10.js
@@ -1,8 +1,15 @@
 function longestPrefix() {
-    let arr = [1,2,3,4]
+    let arr = [1, 2, 3, 4, 5]
+    let arrLen = arr.length;
     let runs = 0;
-    for(let i = 0; i < 3; i++) {
-        for (let j = 0; )
+    let i = 0;
+    for (i; i < arrLen; i++) {
+        let j = i + 1
+        for (j; j < arrLen; j++) {
+            runs++;
+        }
     }
+    return runs;
 }
+
 module.exports = longestPrefix;
--- assets/2020-06-10.test.js
+++ assets/2020-06-10.test.js
@@ -1,8 +1,8 @@
 const longestPrefix = require("./2020-06-10")
 describe('longest prefix', () => {
-    test('given 4 items and needing to combine 2 of them, loop should run 6 times', () => {
+    test('given 5 items and needing to combine 2 of them, loop should run 12 times', () => {
         let result = longestPrefix();
-        expect(result).toEqual(6);
+        expect(result).toEqual(10);
     });
     
 });
