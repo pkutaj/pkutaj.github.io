4afc00f41574168f4a5944eba49c38e0c9b734da
pavol kutaj <pkutaj@gmail.com>
2020-06-24 04:14:12 +0000

    assets

--- assets/2020-06-10.js
+++ assets/2020-06-10.js
@@ -1,15 +1,5 @@
 function longestPrefix(arr) {
-    /* let arr = new Array(arrSize); */
-    let n = arr.length;
-    let runs = 0;
-    let i = 0;
-    for (i; i < n; i++) {
-        let j = i + 1
-        for (j; j < n; j++) {
-            if(arr[i]===)
-        }
-    }
-    return runs;
+if(arr.length === 0) return "";
 }
 
 module.exports = longestPrefix;
--- assets/2020-06-10.test.js
+++ assets/2020-06-10.test.js
@@ -1,21 +1,9 @@
 const longestPrefix = require("./2020-06-10")
- describe('longest prefix', () => {
-/*    test('given 5 items and needing to combine 2 of them, loop should run 12 times', () => {
-        let result = longestPrefix(3);
-        expect(result).toEqual(3);
-    }); */
+describe('longest prefix', () => {
 
-    /* test('given 4 words, it should take it as N and return 6', () => {
-        let input = ["cat", "cap", "pig", "pit"];
-        let result = longestPrefix(input)
-        expect(result).toEqual(6);
-    });
- */
-
-     test('given no equal prefix, it should return an false ', () => {
-        let input = ["cat", "dog"]
-        let result = longestPrefix(input)
-        expect(result).toEqual(false);
-    });
-    
+    test('given empty string, it should return empty string', () => {
+        let arr = []
+        let result = longestPrefix(arr)
+        expect(result).toEqual("");
+    })
 });
